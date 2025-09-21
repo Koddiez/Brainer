454e4b64600d5c590646c6a7406b010b67cedcb4
Koddiez <stevendave847@gmail.com>
2025-09-21 22:29:35 +0100

    Add REST API endpoint stubs

---
+++ src/api/index.ts
@@ -0,0 +1,37 @@
+// src/api/index.ts
+
+// Competitions API Stubs
+export const getCompetitions = () => {
+    // TODO: Implement GET competitions endpoint
+};
+
+export const createCompetition = (data) => {
+    // TODO: Implement POST competition endpoint
+};
+
+// Courses API Stubs
+export const getCourses = () => {
+    // TODO: Implement GET courses endpoint
+};
+
+export const createCourse = (data) => {
+    // TODO: Implement POST course endpoint
+};
+
+// Users API Stubs
+export const getUsers = () => {
+    // TODO: Implement GET users endpoint
+};
+
+export const createUser = (data) => {
+    // TODO: Implement POST user endpoint
+};
+
+// Submissions API Stubs
+export const getSubmissions = () => {
+    // TODO: Implement GET submissions endpoint
+};
+
+export const createSubmission = (data) => {
+    // TODO: Implement POST submission endpoint
+};
